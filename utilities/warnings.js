ee7268d48ea88e97551934ed428b6008d958f23c
Mathieu Huot <mathieu.huot@gmail.com>
2022-08-10 00:18:14 -0400

    Return warning message for easier testing.

--- utilities/warnings.js
+++ utilities/warnings.js
@@ -1,10 +1,16 @@
-module.exports = (target) => {
+module.exports = target => {
+  let warning = "";
+
   Object.keys(target).forEach(property => {
     switch(property) {
       case "alt":
       case "title":
       case "src":
-        console.warn(`WARNING: Setting ${property} in configuration will have no effect on the markdown image output. The ${property} attribute has to be set on the markdown image token.`);
+        const warningMessage = `WARNING: Setting ${property} in configuration will have no effect on the markdown image output. The ${property} attribute has to be set on the markdown image token.`;
+        console.warn(warningMessage);
+        warning = warningMessage;
     }
   });
+
+  return warning;
 }
