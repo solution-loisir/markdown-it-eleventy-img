034eed12727aad1b217dfc3eac97f3152f2b886d
Mathieu Huot <mathieu.huot@gmail.com>
2022-08-11 23:00:11 -0400

    Added more tests.

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
 const test = require("ava");
-const md = require("markdown-it");
+const md = require("markdown-it")();
 const markdownItEleventyImg = require("./");
 const logWarningFor = require("./utilities/warnings");
+const fs = require("fs");
+const path = require("path");
+const Eleventy = require("@11ty/eleventy");
+const eleventyInput = "test-eleventy";
+const eleventyOutput = "_site";
 
 test("Log warning for alt", t => {
   const globalAttributes = {
@@ -29,7 +34,7 @@ test("Log warning for src", t => {
 
 test("typeObjectError for imgOptions (string)", t => {
   t.throws(() => {
-    md().use(markdownItEleventyImg, {
+    md.use(markdownItEleventyImg, {
       imgOptions: ""
     });
   }, {message: "Markdown-it-eleventy-img: `imgOptions` needs to be an `object`."});
@@ -37,7 +42,7 @@ test("typeObjectError for imgOptions (string)", t => {
 
 test("typeObjectError for imgOptions (number)", t => {
   t.throws(() => {
-    md().use(markdownItEleventyImg, {
+    md.use(markdownItEleventyImg, {
       imgOptions: 1
     });
   }, {message: "Markdown-it-eleventy-img: `imgOptions` needs to be an `object`."});
@@ -45,7 +50,7 @@ test("typeObjectError for imgOptions (number)", t => {
 
 test("typeObjectError for imgOptions (array)", t => {
   t.throws(() => {
-    md().use(markdownItEleventyImg, {
+    md.use(markdownItEleventyImg, {
       imgOptions: []
     });
   }, {message: "Markdown-it-eleventy-img: `imgOptions` needs to be an `object`."});
@@ -53,7 +58,7 @@ test("typeObjectError for imgOptions (array)", t => {
 
 test("typeObjectError for imgOptions (null)", t => {
   t.throws(() => {
-    md().use(markdownItEleventyImg, {
+    md.use(markdownItEleventyImg, {
       imgOptions: null
     });
   }, {message: "Markdown-it-eleventy-img: `imgOptions` needs to be an `object`."});
@@ -61,8 +66,95 @@ test("typeObjectError for imgOptions (null)", t => {
 
 test("typeObjectError for imgOptions (undefined not throwing)", t => {
   t.notThrows(() => {
-    md().use(markdownItEleventyImg, {
+    md.use(markdownItEleventyImg, {
       imgOptions: undefined
     });
   });
 });
+
+test.skip("markdownItEleventyImg with markdown-it (default no-config)", t => {
+  const result = md.use(markdownItEleventyImg).render('![Alt diplomees2021](./assets/images/diplomees2021.jpg "Title diplomees2021")');
+
+  t.is(result, '<p><picture><source type="image/webp" srcset="/img/pRWAdktn3m-2048.webp 2048w"><img alt="Alt diplomees2021" title="Title diplomees2021" src="/img/pRWAdktn3m-2048.jpeg" width="2048" height="1463"></picture></p>\n');
+});
+
+test.serial("markdownItEleventyImg with Eleventy (default no-config)", async t => {
+  let elev = new Eleventy(eleventyInput, eleventyOutput, {
+    config(config) {
+      config.setLibrary("md", md.use(markdownItEleventyImg));
+    }
+  });
+  let json = await elev.toJSON();
+  
+  t.is(json[0].content, '<p><picture><source type="image/webp" srcset="/img/pRWAdktn3m-2048.webp 2048w"><img alt="Alt diplomees2021" title="Title diplomees2021" src="/img/pRWAdktn3m-2048.jpeg" width="2048" height="1463"></picture></p>\n');
+});
+
+test.serial("markdownItEleventyImg with Eleventy with imgOptions and globalAttributes (dryrun)", async t => {
+  let elev = new Eleventy(eleventyInput, eleventyOutput, {
+    config(config) {
+      config.setLibrary("md", md.use(markdownItEleventyImg, {
+        imgOptions: {
+          widths: [800, 500, 300],
+          urlPath: "/images/",
+          outputDir: path.join("_site", "images"),
+          formats: ["avif", "webp", "jpeg"],
+          dryRun: true
+        },
+        globalAttributes: {
+          class: "markdown-image",
+          decoding: "async",
+          sizes: "100vw"
+        }
+      }));
+    }
+  });
+  let json = await elev.toJSON();
+  
+  t.is(json[0].content, '<p><picture><source type="image/avif" srcset="/images/pRWAdktn3m-300.avif 300w, /images/pRWAdktn3m-500.avif 500w, /images/pRWAdktn3m-800.avif 800w" sizes="100vw"><source type="image/webp" srcset="/images/pRWAdktn3m-300.webp 300w, /images/pRWAdktn3m-500.webp 500w, /images/pRWAdktn3m-800.webp 800w" sizes="100vw"><source type="image/jpeg" srcset="/images/pRWAdktn3m-300.jpeg 300w, /images/pRWAdktn3m-500.jpeg 500w, /images/pRWAdktn3m-800.jpeg 800w" sizes="100vw"><img class="markdown-image" decoding="async" alt="Alt diplomees2021" title="Title diplomees2021" src="/images/pRWAdktn3m-300.jpeg" width="800" height="571"></picture></p>\n');
+});
+
+test.after("cleanup", t => {
+  const imgDir = "img";
+
+  if(fs.existsSync(imgDir)) {
+    fs.rmSync(imgDir, { recursive: true });
+  }
+});
+
+/**
+ * config.setLibrary('md', markdownIt ({
+    html: true,
+    breaks: true,
+    linkify: true
+  })
+  .use(markdownItAttrs)
+  .use(markdownItEleventyImg, {
+    imgOptions: {
+      widths: [800, 500, 300],
+      urlPath: "/images/",
+      outputDir: path.join("_site", "images"),
+      formats: ["avif", "webp", "jpeg"],
+      dryRun: true
+    },
+    globalAttributes: {
+      class: "markdown-image",
+      decoding: "async",
+      sizes: "100vw",
+      alt: "Uniform alt",
+      title: "Uniform title"
+    },
+    renderImage(image, attributes) {
+      const [ Image, options ] = image;
+      const [ src, attrs ] = attributes;
+
+      Image(src, options);
+
+      const metadata = Image.statsSync(src, options);
+      const imageMarkup = Image.generateHTML(metadata, attrs, {
+        whitespaceMode: "inline"
+      });
+    
+      return `<figure>${imageMarkup}${attrs.title ? `<figcaption>${attrs.title}</figcaption>` : ""}</figure>`;
+    }
+  }));
+ */
