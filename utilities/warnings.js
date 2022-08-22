c6e2ece14f424767862f983742d175a1717bf5b3
Mathieu Huot <mathieu.huot@gmail.com>
2022-08-22 19:57:46 -0400

    Removed warning for `title`.

--- utilities/warnings.js
+++ utilities/warnings.js
@@ -4,7 +4,6 @@ module.exports = target => {
   Object.keys(target).forEach(property => {
     switch(property) {
       case "alt":
-      case "title":
       case "src":
         warning = `Markdown-it-eleventy-img WARNING: Setting \`${property}\` in \`globalAttributes\` will have no effect on the markdown image output. The \`${property}\` attribute has to be set on the markdown image token.`;
         console.warn(warning);
