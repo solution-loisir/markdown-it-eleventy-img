9730bf7817e6701829cc3e8a05b921cf0b09036a
Mathieu Huot <mathieu.huot@gmail.com>
2022-08-21 22:30:13 -0400

    Configured eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2018
+  },
+  rules: {
+    indent: ["error", 2, { "SwitchCase": 1 }],
+    "linebreak-style": ["error", "unix"],
+    semi: ["error", "always"],
+    "no-unused-vars": ["warn"]
+  }
+}
