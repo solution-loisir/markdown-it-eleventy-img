fe426efffc2b7c5e0942579bdba8fdcfb0b5292d
Mathieu Huot <mathieu.huot@gmail.com>
2024-03-09 14:03:49 -0500

    Add comment to remove-key-from.

--- utilities/remove-key-from.js
+++ utilities/remove-key-from.js
@@ -1,5 +1,12 @@
-const remove = (excludeValue = "") => ({
-  from(source = {}) {
+/**
+ * Removes matching property to `excludeValue` from a provided `source` obbject. Returns an object containing the `from` method which takes the `source` object as argument. If `excludeValue` does _not_ exist, it returns the `source` object. If `excludeValue` does exist, it returns a new object without `excludeValue`.
+ * @param {string} excludeValue
+ * @example remove("propname").from(sourceObj);
+ * @returns {{ from(source: {}): {} }}
+ */
+
+const remove = (excludeValue) => ({
+  from(source) {
     if(!source[excludeValue]) return source;
 
     return Object.keys(source).filter(key => key !== excludeValue).reduce((acc, current) => {
