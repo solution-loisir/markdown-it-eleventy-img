327b7f8e7d9580e2f50275bc8db2afada5d6c60e
Mathieu Huot <mathieu.huot@gmail.com>
2024-03-11 07:46:05 -0400

    Grouping types into types.js.

--- index.js
+++ index.js
@@ -5,27 +5,11 @@ const generateAttrsObject = require("./utilities/generate-attrs-object");
 const { typeObjectError, typeFunctionError } = require("./utilities/errors");
 const { propertiesFrom } = require("./utilities/lower-case-trim-object");
 
+/** @typedef {require("type.js").markdownItEleventyImgOptions} markdownItEleventyImgOptions */
 /**
- * @typedef {import("@11ty/eleventy-img").ImageOptions} ImageOptions
- * @link https://github.com/11ty/eleventy-img/blob/d9723c59c94b93cfc05efbffd61efef7d19906e8/img.js#L17
- */
-
-/** @typedef {require("types.js").GlobalAttributes} GlobalAttributes */
-
-/**
- * @typedef markdownItEleventyImgOptions
- * @property {ImageOptions} imgOptions Overrides eleventy-img specific options.
- * @property {GlobalAttributes} globalAttributes Adds attributes to the image output.
- * @property {Function} renderImage Lets you render custom markup and do almost everything you like with your markdown images.
- * @property {Function} resolvePath Function that will be used to resolve paths for images in markdown. Receives image path string and `env` as parameters. Default resolves to CWD.
- */
-
-/**
- * @function markdownItEleventyImg
  * @param {MarkdownIt} md The markdown-it object.
  * @param {markdownItEleventyImgOptions} [markdownItEleventyImgOptions] Optional options object.
  */
-
 module.exports = function markdownItEleventyImg(md, {
   imgOptions = {},
   globalAttributes = {},
--- types.js
+++ types.js
@@ -1,12 +1,25 @@
 /**
- * @typedef GlobalAttributes
- * @property {string} [key]
- */
-
-/**
  * @typedef TokenAttributesObject
  * @property {string} src
  * @property {string} alt
  * @property {string} [title]
  * @property {string} [key]
+ */
+
+/**
+ * @typedef {import("@11ty/eleventy-img").ImageOptions} ImageOptions
+ * @link https://github.com/11ty/eleventy-img/blob/d9723c59c94b93cfc05efbffd61efef7d19906e8/img.js#L17
+ */
+
+/**
+ * @typedef GlobalAttributes
+ * @property {string} [key]
+ */
+
+/**
+ * @typedef markdownItEleventyImgOptions
+ * @property {ImageOptions} imgOptions Overrides eleventy-img specific options.
+ * @property {GlobalAttributes} globalAttributes Adds attributes to the image output.
+ * @property {Function} renderImage Lets you render custom markup and do almost everything you like with your markdown images.
+ * @property {Function} resolvePath Function that will be used to resolve paths for images in markdown. Receives image path string and `env` as parameters. Default resolves to CWD.
  */
