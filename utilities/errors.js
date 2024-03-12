eaaa24448cd5d454be9d180f514e1820e696227f
Mathieu Huot <mathieu.huot@gmail.com>
2024-03-11 22:58:43 -0400

    Add JSDoc to `errors`.

--- utilities/errors.js
+++ utilities/errors.js
@@ -1,3 +1,8 @@
+/**
+ * Throws an error if `parameterValue` is not an object.
+ * @param {any} parameterValue Parameter to test.
+ * @param {string} parameterName Identifies parameter in error message.
+ */
 const typeObjectError = (parameterValue, parameterName = "") => {
   if (
     typeof parameterValue !== "object" ||
@@ -6,6 +11,11 @@ const typeObjectError = (parameterValue, parameterName = "") => {
   ) throw new Error(`Markdown-it-eleventy-img: \`${parameterName}\` needs to be an \`object\`.`);
 };
 
+/**
+ * Throws an error if `parameterValue` is not a function.
+ * @param {any} parameterValue Parameter to test.
+ * @param {string} parameterName Identifies parameter in error message.
+ */
 const typeFunctionError = (parameterValue, parameterName = "") => {
   if(
     typeof parameterValue !== "function" &&
